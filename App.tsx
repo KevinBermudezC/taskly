58c9d630736602037ae11391573ec9a9789b060f
KevinBermudezC <kevinbermudezc@gmail.com>
2024-09-12 10:55:05 -0500

    :lipstick: Create a ShoppingList component and refactor some styles

--- App.tsx
+++ App.tsx
@@ -1,36 +1,14 @@
-import { StyleSheet, Text, View,TouchableOpacity, Alert} from 'react-native';
+import { StyleSheet, View} from 'react-native';
 import {theme} from "./theme";
+import {ShoppingListItem} from "./components/ShoppingListItem";
 
 export default function App() {
-  const handleDelete = () => {
-    Alert.alert("Are you sure you want to delete this?",
-        "It will be gone for good",
-        [
-          {
-            text: "Yes",
-            onPress: ()=> console.log("Ok, Deleting"),
-            style: "destructive"
-          },
-          {
-            text:"Cancel",
-            style: "cancel"
-          }
-        ]
-    );
-  };
+
   return (
     <View style={styles.container}>
-
-      <View style={styles.itemContainer}>
-        <Text style={styles.itemText}>Coffee</Text>
-        <TouchableOpacity
-            style={styles.button}
-            onPress={handleDelete}
-            activeOpacity={0.8}
-        >
-          <Text style={styles.buttonText}>Delete</Text>
-        </TouchableOpacity>
-      </View>
+      <ShoppingListItem name="Coffee" />
+      <ShoppingListItem name="Tea" />
+      <ShoppingListItem name="Sugar" />
     </View>
   );
 }
@@ -40,29 +18,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.colorWhite,
     justifyContent: 'center',
-  },
-  itemContainer: {
-    paddingVertical: 16,
-    paddingHorizontal: 8,
-    borderBottomColor: theme.colorCerulean,
-    borderBottomWidth: 1 ,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  itemText: {
-    fontSize: 18,
-    fontWeight: '200',
-  },
-  button: {
-    backgroundColor: theme.colorBlack,
-    padding: 8,
-    borderRadius: 6,
-  },
-  buttonText:{
-    color: theme.colorWhite,
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    letterSpacing: 1,
   },
 });
